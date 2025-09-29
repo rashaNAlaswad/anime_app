5fb7b4251627e0ee5055f7f0d3846629a2bbee05
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-09-29 15:28:53 +0300

    Enhance category items layout by adding a rating display with star icon

--- lib/features/home/presentation/widgets/category_items.dart
+++ lib/features/home/presentation/widgets/category_items.dart
@@ -1,3 +1,4 @@
+import 'package:anime_ui/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -40,23 +41,71 @@ class CategoryItems extends StatelessWidget {
       height: 290.h,
       child: Column(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(12.r)),
-            child: Image.asset(
-              item.image,
-              width: 180.w,
-              height: 230.h,
-              fit: BoxFit.cover,
+          Expanded(
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(12.r)),
+                  child: Image.asset(
+                    item.image,
+                    width: 180.w,
+                    height: 230.h,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  top: 12.h,
+                  right: 12.w,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 8.w,
+                      vertical: 4.h,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: AppColors.bluePrimary,
+                          size: 16.sp,
+                        ),
+                        horizontalSpace(4),
+                        Text(
+                          item.rating.toString(),
+                          style: AppTextStyles.font16blackSemiBold.copyWith(
+                            fontSize: 12.sp,
+                            color: AppColors.bluePrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          verticalSpace(8),
-          Column(
-            spacing: 4.h,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(item.title, style: AppTextStyles.font14darkBlueBold),
-              Text(item.type, style: AppTextStyles.font12greyMedium),
-            ],
+          Padding(
+            padding: EdgeInsets.only(top: 8.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  item.title,
+                  style: AppTextStyles.font14darkBlueBold,
+                  textAlign: TextAlign.center,
+                ),
+                verticalSpace(4),
+                Text(
+                  item.type,
+                  style: AppTextStyles.font12greyMedium,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ],
       ),
