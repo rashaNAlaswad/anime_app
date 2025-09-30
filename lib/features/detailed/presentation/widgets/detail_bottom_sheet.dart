9513dd3f1252de0d7359807f899862084a035b4e
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-09-30 11:12:08 +0300

    Add DetailBottomSheet widget for displaying action buttons in the detail view

---
+++ lib/features/detailed/presentation/widgets/detail_bottom_sheet.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants/app_assets.dart';
+import '../../../../core/theme/app_colors.dart';
+import '../../../../core/widgets/app_button.dart';
+
+class DetailBottomSheet extends StatelessWidget {
+  const DetailBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.bottomSheetBackground,
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        spacing: 16,
+        children: [
+          Expanded(
+            child: AppButton(
+              text: 'preview',
+              iconPath: AppIcons.preview,
+              isPrimary: false,
+              onTap: () {},
+            ),
+          ),
+          Expanded(
+            child: AppButton(
+              text: 'Watch Now',
+              iconPath: AppIcons.watchNow,
+              isPrimary: true,
+              onTap: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
